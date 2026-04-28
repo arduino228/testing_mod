a92d664399905ca081cdfe367382260605afe299
Boba Fett <nikitosik5556543@gmail.com>
2026-04-28 09:39:37 +0200

    Add new unit plan to additive reconstructor

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,1 +1,10 @@
+const reconstructor = extend(AdditiveReconstructor, "additive-reconstructor", {});
 
+reconstructor.plans.add(
+  new UnitType.UnitPlan(
+    UnitTypes.new_unit_branch_t1,
+    600,
+    ItemStack.with(Items.silicon, 40, Items.graphite, 40),
+    UnitTypes.new_unit_branch_t2
+  )
+);
