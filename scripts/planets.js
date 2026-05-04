0d13d7027c98673a9eb8674236cb8a4d01c79fc7
Boba Fett <nikitosik5556543@gmail.com>
2026-05-04 13:39:40 +0200

    Remove myPlanet configuration from planets.js
    Removed the definition and configuration of 'myPlanet' and its associated settings.

--- scripts/planets.js
+++ scripts/planets.js
@@ -1,31 +1,3 @@
-const myPlanet = new Planet("my-planet", Planets.sun, 1);
-
-myPlanet.generator = extend(SerpuloPlanetGenerator, {});
-myPlanet.meshLoader = () => new HexMesh(myPlanet, 6);
-
-myPlanet.atmosphereColor = Color.valueOf("4da6ff");
-
-myPlanet.startSector = 10;
-myPlanet.accessible = false;
-myPlanet.alwaysUnlocked = true;
-myPlanet.visible = true;
-
-myPlanet.orbitRadius = 60;
-
-myPlanet.defaultEnv = Env.terrestrial;
-
-new SectorPreset("start", myPlanet, 10, {
-    alwaysUnlocked: true,
-    addStartingItems: true,
-    captureWave: 10,
-    difficulty: 1
-});
-
-// 🔥 ключевая строка
-TechTree.nodeRoot("my-planet", myPlanet, () => {});
-//  ОБЯЗАТЕЛЬНО
-//TechTree.node(Planets.serpulo, myPlanet, () => {});
-/*
 // ===== генератор =====
 const myGen = extend(SerpuloPlanetGenerator, {
 
@@ -113,4 +85,4 @@ new SectorPreset("my-sector", myPlanet, 10, {
     difficulty: 3
 });
 
-*/
+
