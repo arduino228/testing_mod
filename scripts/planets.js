f6997b1d060b8d8f8b850b3496e777e6e7542744
Boba Fett <nikitosik5556543@gmail.com>
2026-05-04 13:55:43 +0200

    Refactor planet generator and initialization

--- scripts/planets.js
+++ scripts/planets.js
@@ -1,91 +1,26 @@
-// ===== генератор =====
-const myGen = extend(ErekirPlanetGenerator, {
+Events.run(EventType.ContentInitEvent, () => {
 
-    noise1: new Simplex();
-    noise2: new Simplex();
+    const myPlanet = new Planet("my-planet", Planets.sun, 1);
 
-    generateTile(tile, sector){
+    myPlanet.generator = extend(SerpuloPlanetGenerator, {});
+    myPlanet.meshLoader = () => new HexMesh(myPlanet, 6);
 
-        let x = tile.v.x, y = tile.v.y, z = tile.v.z;
+    myPlanet.atmosphereColor = Color.valueOf("4da6ff");
 
-        let base = this.noise1.octaveNoise3D(4, 0.5, 1/10, x, y, z);
-        let detail = this.noise2.octaveNoise3D(6, 0.6, 1/25, x, y, z);
+    myPlanet.startSector = 10;
+    myPlanet.accessible = true;
+    myPlanet.alwaysUnlocked = true;
+    myPlanet.visible = true;
 
-        let height = base + detail * 0.3;
+    myPlanet.orbitRadius = 60;
+    myPlanet.defaultEnv = Env.terrestrial;
 
-        // вода
-        if(height < 0.3){
-            tile.floor = Blocks.water;
-            return;
-        }
+    new SectorPreset("start", myPlanet, 10, {
+        alwaysUnlocked: true,
+        addStartingItems: true,
+        captureWave: 10
+    });
 
-        // берег
-        if(height < 0.4){
-            tile.floor = Blocks.sand;
-        }
-
-        // равнины
-        else if(height < 0.7){
-            tile.floor = Blocks.grass;
-        }
-
-        // горы
-        else{
-            tile.floor = Blocks.stone;
-
-            if(height > 0.8){
-                tile.block = Blocks.boulder;
-            }
-        }
-
-        // реки
-        let river = Math.abs(this.noise2.octaveNoise3D(1, 1, 1/30, x, y, z));
-        if(river < 0.02 && height > 0.35){
-            tile.floor = Blocks.water;
-        }
-
-    }
-
+    TechTree.nodeRoot("my-planet", myPlanet, () => {});
+    print("PLANET LOADED");
 });
-
-
-// ===== планета =====
-const myPlanet = new Planet("my-planet", Planets.sun, 1);
-
-myPlanet.generator = myGen;
-
-// внешний вид
-myPlanet.meshLoader = () => new NoiseMesh(
-    myPlanet,
-    6,
-    0.6,
-    1.2,
-    1.3,
-    3,
-    Color.valueOf("6ecdec"),
-    Color.valueOf("2b6db2"),
-    Color.valueOf("1c2f5b")
-);
-
-myPlanet.visible = true;
-myPlanet.orbitRadius = 60;
-
-// атмосфера
-myPlanet.atmosphereColor = Color.valueOf("4da6ff");
-myPlanet.atmosphereRadIn = 0.02;
-myPlanet.atmosphereRadOut = 0.3;
-
-// доступ
-myPlanet.startSector = 1;
-myPlanet.alwaysUnlocked = true;
-myPlanet.accessible = true;
-
-
-// ===== сектор =====
-new SectorPreset("my-sector", myPlanet, 1, {
-    alwaysUnlocked: true,
-    captureWave: 15,
-    difficulty: 3
-});
-
-
