9303df45400571986b234ce455cb28fce233ec93
Boba Fett <nikitosik5556543@gmail.com>
2026-05-04 14:04:26 +0200

    Refactor planet initialization and sector setup

--- scripts/planets.js
+++ scripts/planets.js
@@ -1,26 +1,23 @@
-Events.run(EventType.ContentInitEvent, () => {
+const myPlanet = new Planet("my-planet", Planets.sun, 1);
 
-    const myPlanet = new Planet("my-planet", Planets.sun, 1);
+myPlanet.generator = extend(SerpuloPlanetGenerator, {});
+myPlanet.meshLoader = () => new HexMesh(myPlanet, 6);
 
-    myPlanet.generator = extend(SerpuloPlanetGenerator, {});
-    myPlanet.meshLoader = () => new HexMesh(myPlanet, 6);
+myPlanet.atmosphereColor = Color.valueOf("4da6ff");
 
-    myPlanet.atmosphereColor = Color.valueOf("4da6ff");
+myPlanet.startSector = 10;
+myPlanet.accessible = true;
+myPlanet.alwaysUnlocked = true;
+myPlanet.visible = true;
 
-    myPlanet.startSector = 1;
-    myPlanet.accessible = true;
-    myPlanet.alwaysUnlocked = true;
-    myPlanet.visible = true;
+myPlanet.orbitRadius = 60;
+myPlanet.defaultEnv = Env.terrestrial;
 
-    myPlanet.orbitRadius = 60;
-    myPlanet.defaultEnv = Env.terrestrial;
-
-    new SectorPreset("Посев", myPlanet, 1, {
-        alwaysUnlocked: true,
-        addStartingItems: true,
-        captureWave: 10
-    });
-
-    TechTree.nodeRoot("my-planet", myPlanet, () => {});
-    print("PLANET LOADED");
+new SectorPreset("start", myPlanet, 10, {
+    alwaysUnlocked: true,
+    addStartingItems: true,
+    captureWave: 10
 });
+
+//вместо nodeRoot:
+TechTree.node(Planets.sun, myPlanet, () => {});
