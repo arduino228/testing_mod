9b801b7ab70a6bde3489c5caf9c7fd066d82d218
Boba Fett <nikitosik5556543@gmail.com>
2026-05-05 17:06:46 +0200

    Change myStar meshLoader to use StarMesh

--- scripts/myStar.js
+++ scripts/myStar.js
@@ -1,15 +1,19 @@
 const myStar = new Planet("my-star", null, 4);
 
-myStar.radius = 5;
+//myStar.radius = 4;
 
 myStar.bloom = true;
 myStar.accessible = false;
 myStar.visible = true;
 
-myStar.meshLoader = () => new SunMesh(
+myStar.meshLoader = () => new StarMesh(
     myStar,
     5,
-    5, 0.3, 1.7, 1.2, 1,
+    5,
+    0.3,
+    1.7,
+    1.2,
+    1,
     Color.valueOf("ffcc33"),
     Color.valueOf("ff9933"),
     Color.valueOf("ff5500")
