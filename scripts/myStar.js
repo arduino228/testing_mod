fa86a071ce15242925a41bcd60918d2d53016d2c
Boba Fett <nikitosik5556543@gmail.com>
2026-05-05 17:14:45 +0200

    Update myStar properties and mesh loader

--- scripts/myStar.js
+++ scripts/myStar.js
@@ -1,20 +1,21 @@
 const myStar = new Planet("my-star", null, 4);
 
-//myStar.radius = 4;
+myStar.radius = 6;
+myStar.orbitRadius = 0;
 
 myStar.bloom = true;
+myStar.lightRadius = 30;
+
 myStar.accessible = false;
 myStar.visible = true;
 
-myStar.meshLoader = () => new StarMesh(
+myStar.meshLoader = () => new SunMesh(
     myStar,
     5,
-    5,
-    0.3,
-    1.7,
-    1.2,
-    1,
+    5, 0.3, 1.7, 1.2, 1,
     Color.valueOf("ffcc33"),
     Color.valueOf("ff9933"),
     Color.valueOf("ff5500")
 );
+
+TechTree.nodeRoot("my-star", myStar, () => {});
